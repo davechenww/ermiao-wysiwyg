724acf12782e242b8ae95b615e32f50b452c8b3b
davechenww <dave.chen.ww@gmail.com>
2014-10-21 18:39:46 +0800

    Add summernote editor test view.

---
+++ website/views.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.views.generic import View
+
+
+class Index(View):
+
+    def get(self, request, *args, **kwargs):
+        return render_to_response('index.html', {}, RequestContext(request))
