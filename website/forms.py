3f3f834c45ce4e949e394bd3b7adb3c9ffd7394c
davechenww <dave.chen.ww@gmail.com>
2014-10-24 18:48:34 +0800

    Fix: main_image change to cover, index and detail page style.

--- website/forms.py
+++ website/forms.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 from django import forms
 
+
 class TextForm(forms.Form):
     title = forms.CharField(max_length=50)
-    text = forms.CharField(min_length=2, 
-        error_messages={'min_length':u'这项要填哦!!'} ) 
-    main_image = forms.CharField(required=False)
+    text = forms.CharField()
+    cover = forms.CharField(required=False)
